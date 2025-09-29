9bd667779a57918f2187d54f4493424cbb8328d8
Rimuru-nis <satroug36@gmail.com>
2025-09-29 23:44:27 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,11 @@
+// console.log("nishant")
+age = 24;
+x = null;
+y = undefined;
+console.log(x)
+
+let firstname= "Nishant";
+let lastName= "Kumar";
+console.log(firstname);
+console.log(lastName);
+console.log(firstname +" "+ lastName)
